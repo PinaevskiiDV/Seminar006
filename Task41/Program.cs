b127e1b350807aec3b25feba26a51cf048de6705
Dmitrii <pinochet37@mail.ru>
2022-09-19 16:33:11 +0300

    aded Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+    int m;
+    Console.WriteLine("Введите число элементов массива");
+    m = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[m];
+    int i;
+    for (i = 0; i < m; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+Console.WriteLine($"В заданном массиве {PozitiveElement(array)} чисел больше 0");
+
+double PozitiveElement(int[] array)
+{
+    int result = 0;
+    int j;
+    for(j = 0; j < m; j++)
+    {
+        if(array[j] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
