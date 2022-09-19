a1648045bc2868b9774313cf1f7264a82215f2f7
Dmitrii <pinochet37@mail.ru>
2022-09-19 18:21:13 +0300

    aded Task43

---
+++ Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите b1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2");
+int d = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"Точка пересечения заданных прямых расположена в координатах {(FindX(a, b, c, d), FindY(a, b, c, d))}");
+
+float FindX(float a, float b, float c, float d)
+{
+    float x = -1 * (a - c) / (b - d);
+    return x;
+}
+
+float FindY(float a, float b, float c, float d)
+{
+    float y = b * (-1 * (a - c) / (b - d)) + a;
+    return y;
+}
